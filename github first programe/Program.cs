3d874bf35191e0921b4bba28bf70eb06bbf73905
TEJAS <TEJAS@TGREX>
2024-10-06 13:24:48 +0530

    Hello World is now added

---
+++ github first programe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_first_programe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
